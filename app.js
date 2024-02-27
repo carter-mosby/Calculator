7418ee0796424482b1c8cfb3892a6f17e40ed45e
carter-mosby <cartermosby12@gmail.com>
2024-02-27 13:24:19 -0700

    debugging

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ class calculator {
         this.currentOperand = "";
         this.previousOperand = "";
         this.operation = undefined;
+        this.sciFunction = undefined;
     }
 
     delete(){
@@ -16,9 +17,19 @@ class calculator {
     }
 
     appendNumber(number){
-        if(number === '.' && this.currentOperand.includes('.')) return
-        this.currentOperand = this.currentOperand.toString() + number.toString()
+        if(number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
     }
+
+
+    // bugged; with this method im trying to allow appendage to the current function which is clicked. 
+    appendSciFunc(funct) {
+        if (isNaN(parseFloat(this.currentOperand)) && funct !== '') {
+            this.currentOperand += funct;
+        }
+        this.updateDisplay();
+    }
+    
 
     chooseOperation(operation){
         if(this.currentOperand === '') return
@@ -30,7 +41,19 @@ class calculator {
         this.currentOperand = '';
     }
 
+    // bugged; when a function is clicked i want it to appear in the current text field, once clicked it should allow for the SciFunccomputation method;
+    // see sciFuncComputation method for more info
+    chooseSciFunc(sciFunction){
+        if(this.previousOperand === '') return;
+        if (this.currentOperand === ''){
+            this.sciFuncComputation();
+        };
+        this.sciFunction = sciFunction;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand ='';
+    }
 
+//computation method which parses the current and previous opperand and turns out a calculatrion based on if its in string format or not
     compute() {
         let computation
         const prev = parseFloat(this.previousOperand);
@@ -55,9 +78,49 @@ class calculator {
         this.currentOperand = computation;
         this.operation = undefined;    
         this.previousOperand = ''
+
     }
 
+    // this should allow the appending of a number to the current operand. once appended it should push the result to the previousOperandTextElement
+    sciFuncComputation(){
+        const current = parseFloat(this.currentOperand); //parses the current operand and saves it to a variable so the built in Math function accepts
+        //numbers appended to the current science function, although im doubtful this is useful and could just pass in the currentOperand. 
+        if(isNaN(current)) return; 
 
+            switch(this.sciFunction){
+                case"tan":
+                    Math.tan(current)
+                    break;
+
+                case"sin":
+                    Math.sin(current)
+                    break;
+
+                case"cos":
+                    Math.cos(current)
+                    break;
+
+                case"tanH":
+                    Math.tanh(current)
+                    break;
+
+                case"sinH":
+                    Math.sinh(current)
+                    break;
+
+                case"cosH":
+                    Math.cosh(current)
+                    break;
+
+                default:
+                    return; 
+            }
+            this.currentOperand = '';
+            this.sciFunction = undefined;
+            this.previousOperand = this.currentOperand;
+    }
+
+//this method allows for a single digit point and then numbers to be added at the end of it without allowing for repition of a decimal point
     getDisplayNumber(number){
         const stringNumber = number.toString();
         const integerDigits = parseFloat(stringNumber.split('.')[0])
@@ -77,22 +140,35 @@ class calculator {
             return integerDisplay;
         }
     } 
-    
+//display method : bugged; it displays numbers but doesnt push to the previous text element, when an operation is clicked.
+//when operation is pressed it clears Both text fields and then reappears when new numbers are pressed; the compute function still works.  
+
     updateDisplay(){
         this.currentOperandTextElement.innerText = 
             this.getDisplayNumber(this.currentOperand);
         if(this.operation != null){
             this.previousOperandTextElement.innerText = 
-                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}` 
         } else{
+            this.previousOperandTextElement.innerText = '';
+        }
+
+        this.currentOperandTextElement.innerText =
+            this.currentOperand;
+        if (this.sciFunction != null){
+            this.currentOperandTextElement.innerText =
+                `${this.sciFunction} ${this.getDisplayNumber(this.currentOperand)}`
+        } else {
             this.previousOperandTextElement.innerText = '';
         }
     }
 }
 
 
-
-const sciFunc = document.querySelectorAll('[data-sciFunc]')
+//I used data operators for legibility and organization 
+const radMode = document.querySelector('[scirdsettingr]')
+const degMode = document.querySelector('[scirdsettinfd]')
+const functionButtons = document.querySelectorAll('[data-sciFunc]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const numberButtons = document.querySelectorAll('[data-number]');
@@ -118,6 +194,7 @@ operationButton.forEach(button=> {
 })
 
 equalsButton.addEventListener('click', button => {
+    Calculator.sciFuncComputation();
     Calculator.compute();
     Calculator.updateDisplay();
 }) 
@@ -130,5 +207,13 @@ allClearButton.addEventListener('click', button => {
 deleteButton.addEventListener('click', button => {
     Calculator.delete();
     Calculator.updateDisplay();
+})
+
+functionButtons.forEach(button=> {
+    button.addEventListener('click', ()=> {
+        Calculator.chooseSciFunc(button.innerText);
+        Calculator.appendSciFunc(); //should this be here? im thinking i should place it in the  number event Listener so it can append to the science function.
+        Calculator.updateDisplay();
+    })
 })
 
