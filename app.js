f32001a0dc240cbaea9d5688b8fe9b79aa53a950
carter-mosby <cartermosby12@gmail.com>
2024-03-04 13:05:50 -0700

    fixed bugs, added comments

--- app.js
+++ app.js
@@ -4,27 +4,33 @@ class calculator {
         this.currentOperandTextElement = currentOperandTextElement;
         this.clear();
     } 
-
+    //clears calculator
     clear(){
         this.currentOperand ='';
         this.previousOperand ='';
         this.operation = undefined;
         this.sciFunction = undefined;
     };
-
+    //deletes current && previous operand
     delete(){
+        if(this.currentOperand !== ''){
         this.currentOperand = this.currentOperand.toString().slice(0, -1);
-        this.previousOperand = this.previousOperand.toString().slice(0, -1);
+        }else if (this.previousOperand !=='' && this.operation !== ''){
+        this.previousOperand = ''
+        this.operation = ''
+        }
     }
-
+    //allows for one comma to be appended, then converts the number typed in to a string
     appendNumber(number){
         if(number === '.' && this.currentOperand.includes('.')) return;
         this.currentOperand = this.currentOperand.toString() + number.toString();
     }
 
-
+    //sets an operator to the end of string
     chooseOperation(operation){
+        //if current operand is = null then doesnt allow for an operator to be placed
         if(this.currentOperand === '') return
+        //when both fields are complete with a string allows the computation function to be ran
         if(this.previousOperand !== ''){
             this.compute();
         };
@@ -32,7 +38,7 @@ class calculator {
         this.previousOperand = this.currentOperand;
         this.currentOperand = '';
     }
-
+    //allows for choice of a sci operator 
      chooseSciOperator(sciFunction){
         if(this.currentOperand === ''){
             this.compute();
@@ -44,8 +50,9 @@ class calculator {
     }
 
     
-
+    //computes the current strings by turning current strings back into numbers with parse float
     compute() {
+        //defines completed variable
         let computation
         const prev = parseFloat(this.previousOperand);
         const current = parseFloat(this.currentOperand);
@@ -66,34 +73,44 @@ class calculator {
             default:
                 return 
         }
+        //once completed turns completed variable back to the currennt opperand
         this.currentOperand = computation;
         this.operation = undefined;    
-        this.previousOperand = Enumerator;
+        this.previousOperand = '';
 
     }
 
     
     getDisplayNumber(number){
+        //turns number to string
         const stringNumber = number.toString();
+        //if no decimal is found before number it is saved to integer digit and returned as a number
         const integerDigits = parseFloat(stringNumber.split('.')[0])
+        //if decimal point is present saves numbers after to decimal digit
         const decimalDigits = stringNumber.split('.')[1]
+
         let integerDisplay = '';
+
          if(integerDigits){ 
+            //turns the current display to a localestring in this case En so it seperates the current integer with commas when exceding a certain length 
+            //(i.e 100000 V.S. 100,000) for legibility
             integerDisplay = integerDigits.toLocaleString('en', {
                 maximumFractionDigits: 0
             })
         }
-
+        //if decimal digits are present; appends the decimal digits without formation to the now formmated integer digits.
         if (decimalDigits != null){
             return `${integerDisplay}.${decimalDigits}`;
         } else{
             return integerDisplay;
-           }
+        }
     }  
-
+    //this method allows for computation of the scientifc functions
     sciFuncComputation(){
         let sciComp
-        const current = parseInt(this.currentOperand);
+        //turns the current opperand to a string
+        const current = parseFloat(this.currentOperand);
+        //uses built-in statements in the switch case
             switch(this.sciFunction){
                 case"tan":
                     sciComp = Math.tan(current);
@@ -148,8 +165,11 @@ class calculator {
     } 
 
     updateDisplay(){
+        //allows display of the current opperand text element by calling get display number
         this.currentOperandTextElement.innerText = 
             this.getDisplayNumber(this.currentOperand);
+            //if the operation button is clicked and new numbers are added, pushes the current working space to previous working space; and allows for 
+            //new numbers to be added. 
         if(this.operation != null){
             this.previousOperandTextElement.innerText = 
                 `${this.getDisplayNumber(this.previousOperand)} ${this.operation}` 
@@ -158,19 +178,19 @@ class calculator {
         }
         
         
-        
+        //for the scientific computation method 
+        //keeps scientific function in current working space until equals event listener is executed and returns the result of the operation. 
         if (this.sciFunction != null){
             this.currentOperandTextElement.innerText = 
+            //allows appendage of the scientific element
            `${this.sciFunction}(${this.getDisplayNumber(this.currentOperand)})`
         } 
         
     }
-}
-
-
-init = () => {
-    const radMode = document.querySelector('[scirdsettingr]');
-    const degMode = document.querySelector('[scirdsettinfd]');
+};
+    //query selectors to grab data from DOM
+    //const radMode = document.querySelector('[scirdsettingr]');
+    //const degMode = document.querySelector('[scirdsettinfd]');
     const functionButtons = document.querySelectorAll('[data-sciFunc]')
     const currentOperandTextElement = document.querySelector('[data-current-operand]');
     const previousOperandTextElement = document.querySelector('[data-previous-operand]');
@@ -179,10 +199,13 @@ init = () => {
     const deleteButton = document.querySelector('[data-delete]');
     const allClearButton = document.querySelector('[data-all-clear]');
     const operationButton = document.querySelectorAll('[data-operation]');
-    const exponentButton = document.querySelectorAll(['data-sciExponent']);
-    const squareButton = document.querySelectorAll(['data-sciRoot']);
+    //const exponentButton = document.querySelectorAll(['data-sciExponent']);
+    //const squareButton = document.querySelectorAll(['data-sciRoot']);
 
-    const Calculator = new calculator(previousOperandTextElement, currentOperandTextElement);
+
+const Calculator = new calculator(previousOperandTextElement, currentOperandTextElement);
+
+init = () => {
 
     numberButtons.forEach(button => {
         button.addEventListener('click', () => {
@@ -228,5 +251,7 @@ init = () => {
     })
 };
 
-exports.calculate = calculator;
+
+//test exports
+exports = calculator;
 init();
--- test/test.js
+++ test/test.js
@@ -1,9 +1,16 @@
+/*
+const chai = import('chai');
+const expect = chai.expect;
+const calculator =  require('../app');
+var server = "http://127.0.0.1:5500/"
+var calc = new calculator(previousOperandTextElement, currentOperandTextElement);
 
-const assert = require('assert');
-const calculator =  require('../app').calculator;
-const require = require('request');
-
-describe('calculator', function (){
-    var url = "http://127.0.0.1:5500/"; 
-
-})
+describe('testing calculator', function (){
+    describe('computation', function(){
+        it( "testing computation method",function(init){
+        
+     });
+    
+    });
+});
+*/
